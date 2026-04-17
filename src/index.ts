4ddc1d56177164831d76298e4c593cbba8f57b1b
Veljko Naumovic <veljko.naumovic@gmail.com>
2026-04-18 00:38:51 +0200

    fix CORS config

--- src/index.ts
+++ src/index.ts
@@ -8,7 +8,7 @@ const app = express();
 
 app.use(
 	cors({
-		origin: process.env.FRONTEND_URL || "*",
+		origin: ["http://localhost:5173", process.env.FRONTEND_URL].filter(Boolean) as string[];,
 	}),
 );
 
